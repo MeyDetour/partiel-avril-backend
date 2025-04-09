ac443b5831463cd036efc2ee063ab65464d5358c
mey <meydetour@gmail.com>
2025-04-09 14:38:48 +0200

    fix: downlaod pdf

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -138,7 +138,7 @@ final class ProductController extends AbstractController
         $html = "<html><body style='margin:0'><img src='" . $product->getQrCodeUrl() . "' style='width:100%;height:auto'/></body></html>";
 
         return new PdfResponse(
-            $knpSnappyPdf->generateFromHtml($html),
+            $knpSnappyPdf->getOutputFromHtml($html),
             'file.pdf'
         );
     }
