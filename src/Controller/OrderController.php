059ae2b2c44141d7140b91bfc859d9cd8d60a4f3
mey <meydetour@gmail.com>
2025-04-09 10:23:48 +0200

    feat : add delete order function (for debug)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,11 +18,21 @@ final class OrderController extends AbstractController
             "orders" => $orderRepository->findAll()
         ]);
     }
+
     #[Route('/order/{id}', name: 'app_order')]
     public function show(Order $order): Response
     {
         return $this->render('admin/order/show.html.twig', [
-"order"=>$order
+            "order" => $order
         ]);
+    }
+
+    //USED ONLY FOR DEBUG
+    #[Route('/delete/{id}', name: 'delete_order', methods: "DELETE")]
+    public function delete(Order $order, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($order);
+        $entityManager->flush();
+        return $this->redirectToRoute("app_orders_index");
     }
 }
