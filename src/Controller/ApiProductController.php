312401d623f1116245a8615067b8a015d61d60c7
mey <meydetour@gmail.com>
2025-04-09 08:13:50 +0200

    feat: add get product for api

--- src/Controller/ApiProductController.php
+++ src/Controller/ApiProductController.php
@@ -13,9 +13,17 @@ final class ApiProductController extends AbstractController
 {
 
 
-    #[Route(path: "/all", name: 'app_api_product', methods: "GET")]
+    #[Route(path: "/all", name: 'app_api_products', methods: "GET")]
     public function index(ProductRepository $productRepository): Response
     {
         return $this->json($productRepository->findAll(), 200, [], ["groups" => ["products"]]);
+    }
+
+    #[Route(path: "/{id}", name: 'app_api_product', methods: "GET")]
+    public function getProduct(ProductRepository $productRepository,$id): Response
+    {
+        $product = $productRepository->find($id);
+        if (!$product)return $this->json(["message"=>"Product not found !"],404);
+        return $this->json($product, 200, [], ["groups" => ["products"]]);
     }
 }
